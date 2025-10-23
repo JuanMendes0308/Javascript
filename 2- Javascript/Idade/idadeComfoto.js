8765729790a69861a6af285fdd970638a8adce21
JuanMendes0308 <mendesj0308@gmail.com>
2025-10-23 17:24:17 -0300

    Envio de repositório

---
+++ 2- Javascript/Idade/idadeComfoto.js
@@ -0,0 +1,10 @@
+function Idade(){
+    let corpo = window.document.getElementById('corpo')
+    let imagem = window.document.getElementById('alguem')
+    let resposta = window.document.getElementById('resp')
+    let idade = window.document.getElementById('idade')
+
+    let anos = Number(idade.value)
+
+    
+}
